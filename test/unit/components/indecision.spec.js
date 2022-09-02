a2e9d921b9bfb2997577cc64b38aa8ac37169f8d
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-02 12:21:27 -0300

    se agrego beforeEach()

--- test/unit/components/indecision.spec.js
+++ test/unit/components/indecision.spec.js
@@ -1,16 +1,25 @@
-import { describe, test, expect } from 'vitest'
+import { describe, test, expect, beforeEach } from 'vitest'
 import { shallowMount } from '@vue/test-utils'
 
 import Indecision from '../../../src/components/Indecision.vue'
 
 
 describe('Indecision component', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallowMount(Indecision)
+    })
+
     test('debe de ser identico con el snapshot', () => {
-        const wrapper = shallowMount(Indecision)
         // toMatchSnapshot() garantiza que un valor coincida con la instantánea más reciente.
         expect(wrapper.html()).toMatchSnapshot()
 
         // actualizar snapshot
         // yarn test -u
+    })
+
+    test('p debe tener el valor por defecto "Recuerda terminar con un signo de interrogacion"', () => {
+        const parrafo = wrapper.find('p')
+        expect(parrafo.text()).toBe('Recuerda terminar con un signo de interrogacion')
     })
 })
